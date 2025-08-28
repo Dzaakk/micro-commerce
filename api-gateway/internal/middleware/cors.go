eb633253dd5d84167aab246bdb099b3c8e011aaa
Dzkzzz <dzdzaakk@gmail.com>
2025-08-28 22:20:08 +0700

    cors

---
+++ api-gateway/internal/middleware/cors.go
@@ -0,0 +1,20 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, x-Requested with")
+		c.Writer.Header().Set("Access-Control-MAx-Age", "86400")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+
+}
